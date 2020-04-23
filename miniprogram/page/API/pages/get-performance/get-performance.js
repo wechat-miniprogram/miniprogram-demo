2c53141abf2e9108df496ae6459e0f6a593cbc6b
RomChung <thelns@vip.qq.com>
2020-04-23 23:37:57 +0800

    add samples: getPerformance

--- miniprogram/page/API/pages/get-performance/get-performance.js
+++ miniprogram/page/API/pages/get-performance/get-performance.js
@@ -1,7 +1,39 @@
 // miniprogram/page/API/pages/get-performance/get-performance.js
+
+const util = require('./util')
+const performance = wx.getPerformance();
+const performanceObserver = performance.createObserver();
+
 Page({
-  onShow() {
-    const performance = wx.getPerformance();
-    console.log(performance);
+
+  data: {
+    array: []
+  },
+  getPerformanceInfo() {
+    const EntryList = performance.getEntries();
+    const array = [];
+    EntryList.forEach((item) => {
+      array.push({
+        entryType: util.renderEntryType(item.entryType),
+        name: util.renderName(item.name),
+        duration: util.renderDuration(item.duration),
+        startTime: util.renderStartTime(item.startTime),
+      })
+    })
+    this.setData({
+      array: array,
+    })
+
+  },
+
+  startObserver() {
+    // 监听需要的性能指标
+    performanceObserver.observe({ entryTypes: ['render', 'script', 'navigation'] });
+    
+  },
+
+  stopObserver() {
+    // 结束监听
+    performanceObserver.disconnect();
   }
 })
