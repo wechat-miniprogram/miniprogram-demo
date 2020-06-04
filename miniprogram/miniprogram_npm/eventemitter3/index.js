8f6d352d5b69306794c0c8320aa9429756d58fc4
RomChung <thelns@vip.qq.com>
2020-06-04 11:43:57 +0800

    fix: npm 依赖位置

--- miniprogram/miniprogram_npm/eventemitter3/index.js
+++ miniprogram/miniprogram_npm/eventemitter3/index.js
@@ -1,10 +1,10 @@
 module.exports = (function() {
 var __MODS__ = {};
-var __DEFINE__ = function(modId, func, req) { var m = { exports: {} }; __MODS__[modId] = { status: 0, func: func, req: req, m: m }; };
-var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = { exports: {} }; __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); if(typeof m.exports === "object") { __MODS__[modId].m.exports.__proto__ = m.exports.__proto__; Object.keys(m.exports).forEach(function(k) { __MODS__[modId].m.exports[k] = m.exports[k]; var desp = Object.getOwnPropertyDescriptor(m.exports, k); if(desp && desp.configurable) Object.defineProperty(m.exports, k, { set: function(val) { __MODS__[modId].m.exports[k] = val; }, get: function() { return __MODS__[modId].m.exports[k]; } }); }); if(m.exports.__esModule) Object.defineProperty(__MODS__[modId].m.exports, "__esModule", { value: true }); } else { __MODS__[modId].m.exports = m.exports; } } return __MODS__[modId].m.exports; };
+var __DEFINE__ = function(modId, func, req) { var m = { exports: {}, _tempexports: {} }; __MODS__[modId] = { status: 0, func: func, req: req, m: m }; };
+var __REQUIRE__ = function(modId, source) { if(!__MODS__[modId]) return require(source); if(!__MODS__[modId].status) { var m = __MODS__[modId].m; m._exports = m._tempexports; var desp = Object.getOwnPropertyDescriptor(m, "exports"); if (desp && desp.configurable) Object.defineProperty(m, "exports", { set: function (val) { if(typeof val === "object" && val !== m._exports) { m._exports.__proto__ = val.__proto__; Object.keys(val).forEach(function (k) { m._exports[k] = val[k]; }); } m._tempexports = val }, get: function () { return m._tempexports; } }); __MODS__[modId].status = 1; __MODS__[modId].func(__MODS__[modId].req, m, m.exports); } return __MODS__[modId].m.exports; };
 var __REQUIRE_WILDCARD__ = function(obj) { if(obj && obj.__esModule) { return obj; } else { var newObj = {}; if(obj != null) { for(var k in obj) { if (Object.prototype.hasOwnProperty.call(obj, k)) newObj[k] = obj[k]; } } newObj.default = obj; return newObj; } };
 var __REQUIRE_DEFAULT__ = function(obj) { return obj && obj.__esModule ? obj.default : obj; };
-__DEFINE__(1572960819413, function(require, module, exports) {
+__DEFINE__(1591242034685, function(require, module, exports) {
 
 
 var has = Object.prototype.hasOwnProperty
@@ -343,6 +343,6 @@ if ('undefined' !== typeof module) {
 }
 
 }, function(modId) {var map = {}; return __REQUIRE__(map[modId], modId); })
-return __REQUIRE__(1572960819413);
+return __REQUIRE__(1591242034685);
 })()
 //# sourceMappingURL=index.js.map
--- miniprogram/miniprogram_npm/wxml-to-canvas/index.js
+++ miniprogram/miniprogram_npm/wxml-to-canvas/index.js
@@ -91,17 +91,83 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ (function(module, exports) {
+
+const hex = (color) => {
+  let result = null
+
+  if (/^#/.test(color) && (color.length === 7 || color.length === 9)) {
+    return color
+    // eslint-disable-next-line no-cond-assign
+  } else if ((result = /^(rgb|rgba)\((.+)\)/.exec(color)) !== null) {
+    return '#' + result[2].split(',').map((part, index) => {
+      part = part.trim()
+      part = index === 3 ? Math.floor(parseFloat(part) * 255) : parseInt(part, 10)
+      part = part.toString(16)
+      if (part.length === 1) {
+        part = '0' + part
+      }
+      return part
+    }).join('')
+  } else {
+    return '#00000000'
+  }
+}
+
+const splitLineToCamelCase = (str) => str.split('-').map((part, index) => {
+  if (index === 0) {
+    return part
+  }
+  return part[0].toUpperCase() + part.slice(1)
+}).join('')
+
+const compareVersion = (v1, v2) => {
+  v1 = v1.split('.')
+  v2 = v2.split('.')
+  const len = Math.max(v1.length, v2.length)
+  while (v1.length < len) {
+    v1.push('0')
+  }
+  while (v2.length < len) {
+    v2.push('0')
+  }
+  for (let i = 0; i < len; i++) {
+    const num1 = parseInt(v1[i], 10)
+    const num2 = parseInt(v2[i], 10)
+
+    if (num1 > num2) {
+      return 1
+    } else if (num1 < num2) {
+      return -1
+    }
+  }
+
+  return 0
+}
+
+module.exports = {
+  hex,
+  splitLineToCamelCase,
+  compareVersion
+}
+
+
+/***/ }),
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 
-const xmlParse = __webpack_require__(1)
-const {Widget} = __webpack_require__(2)
+const xmlParse = __webpack_require__(2)
+const {Widget} = __webpack_require__(3)
 const {Draw} = __webpack_require__(5)
+const {compareVersion} = __webpack_require__(0)
+
+const canvasId = 'weui-canvas'
 
 Component({
   properties: {
@@ -114,32 +180,42 @@ Component({
       value: 300
     }
   },
+  data: {
+    use2dCanvas: false, // 2.9.2 后可用canvas 2d 接口
+  },
   lifetimes: {
     attached() {
-      const dpr = wx.getSystemInfoSync().pixelRatio
-      const query = this.createSelectorQuery()
+      const {SDKVersion, pixelRatio: dpr} = wx.getSystemInfoSync()
+      const use2dCanvas = compareVersion(SDKVersion, '2.9.2') >= 0
       this.dpr = dpr
-      query.select('#canvas')
-        .fields({node: true, size: true})
-        .exec(res => {
-          const canvas = res[0].node
-          const ctx = canvas.getContext('2d')
-          canvas.width = res[0].width * dpr
-          canvas.height = res[0].height * dpr
-          ctx.scale(dpr, dpr)
-          this.ctx = ctx
-          this.canvas = canvas
-        })
+      this.setData({use2dCanvas}, () => {
+        if (use2dCanvas) {
+          const query = this.createSelectorQuery()
+          query.select(`#${canvasId}`)
+            .fields({node: true, size: true})
+            .exec(res => {
+              const canvas = res[0].node
+              const ctx = canvas.getContext('2d')
+              canvas.width = res[0].width * dpr
+              canvas.height = res[0].height * dpr
+              ctx.scale(dpr, dpr)
+              this.ctx = ctx
+              this.canvas = canvas
+            })
+        } else {
+          this.ctx = wx.createCanvasContext(canvasId, this)
+        }
+      })
     }
   },
   methods: {
     async renderToCanvas(args) {
       const {wxml, style} = args
-
-      // 清空画布
       const ctx = this.ctx
       const canvas = this.canvas
-      if (!ctx || !canvas) {
+      const use2dCanvas = this.data.use2dCanvas
+
+      if (use2dCanvas && !canvas) {
         return Promise.reject(new Error('renderToCanvas: fail canvas has not been created'))
       }
 
@@ -154,29 +230,50 @@ Component({
         width: container.computedStyle.width,
         height: container.computedStyle.height,
       }
-      const draw = new Draw(canvas, ctx)
+      const draw = new Draw(ctx, canvas, use2dCanvas)
       await draw.drawNode(container)
+
+      if (!use2dCanvas) {
+        await this.canvasDraw(ctx)
+      }
       return Promise.resolve(container)
     },
 
+    canvasDraw(ctx, reserve) {
+      return new Promise(resolve => {
+        ctx.draw(reserve, () => {
+          resolve()
+        })
+      })
+    },
+
     canvasToTempFilePath(args = {}) {
+      const use2dCanvas = this.data.use2dCanvas
+
       return new Promise((resolve, reject) => {
         const {
           top, left, width, height
         } = this.boundary
-        wx.canvasToTempFilePath({
+
+        const copyArgs = {
           x: left,
           y: top,
           width,
           height,
           destWidth: width * this.dpr,
           destHeight: height * this.dpr,
-          canvas: this.canvas,
+          canvasId,
           fileType: args.fileType || 'png',
           quality: args.quality || 1,
           success: resolve,
           fail: reject
-        })
+        }
+
+        if (use2dCanvas) {
+          delete copyArgs.canvasId
+          copyArgs.canvas = this.canvas
+        }
+        wx.canvasToTempFilePath(copyArgs, this)
       })
     }
   }
@@ -184,7 +281,7 @@ Component({
 
 
 /***/ }),
-/* 1 */
+/* 2 */
 /***/ (function(module, exports) {
 
 
@@ -354,11 +451,11 @@ module.exports = parse
 
 
 /***/ }),
-/* 2 */
+/* 3 */
 /***/ (function(module, exports, __webpack_require__) {
 
-const Block = __webpack_require__(3)
-const {splitLineToCamelCase} = __webpack_require__(4)
+const Block = __webpack_require__(4)
+const {splitLineToCamelCase} = __webpack_require__(0)
 
 class Element extends Block {
   constructor(prop) {
@@ -441,57 +538,20 @@ module.exports = {Widget}
 
 
 /***/ }),
-/* 3 */
-/***/ (function(module, exports) {
-
-module.exports = require("widget-ui");
-
-/***/ }),
 /* 4 */
 /***/ (function(module, exports) {
 
-const hex = (color) => {
-  let result = null
-
-  if (/^#/.test(color) && (color.length === 7 || color.length === 9)) {
-    return color
-    // eslint-disable-next-line no-cond-assign
-  } else if ((result = /^(rgb|rgba)\((.+)\)/.exec(color)) !== null) {
-    return '#' + result[2].split(',').map((part, index) => {
-      part = part.trim()
-      part = index === 3 ? Math.floor(parseFloat(part) * 255) : parseInt(part, 10)
-      part = part.toString(16)
-      if (part.length === 1) {
-        part = '0' + part
-      }
-      return part
-    }).join('')
-  } else {
-    return '#00000000'
-  }
-}
-
-const splitLineToCamelCase = (str) => str.split('-').map((part, index) => {
-  if (index === 0) {
-    return part
-  }
-  return part[0].toUpperCase() + part.slice(1)
-}).join('')
-
-module.exports = {
-  hex,
-  splitLineToCamelCase
-}
-
+module.exports = require("widget-ui");
 
 /***/ }),
 /* 5 */
 /***/ (function(module, exports) {
 
 class Draw {
-  constructor(canvas, context) {
-    this.canvas = canvas
+  constructor(context, canvas, use2dCanvas = false) {
     this.ctx = context
+    this.canvas = canvas || null
+    this.use2dCanvas = use2dCanvas
   }
 
   roundRect(x, y, w, h, r, fill = true, stroke = false) {
@@ -540,6 +600,7 @@ class Draw {
     await new Promise((resolve, reject) => {
       const ctx = this.ctx
       const canvas = this.canvas
+
       const {
         borderRadius = 0
       } = style
@@ -549,14 +610,46 @@ class Draw {
       ctx.save()
       this.roundRect(x, y, w, h, borderRadius, false, false)
       ctx.clip()
-      const Image = canvas.createImage()
-      Image.onload = () => {
-        ctx.drawImage(Image, x, y, w, h)
-        ctx.restore()
-        resolve()
+
+      const _drawImage = (img) => {
+        if (this.use2dCanvas) {
+          const Image = canvas.createImage()
+          Image.onload = () => {
+            ctx.drawImage(Image, x, y, w, h)
+            ctx.restore()
+            resolve()
+          }
+          Image.onerror = () => { reject(new Error(`createImage fail: ${img}`)) }
+          Image.src = img
+        } else {
+          ctx.drawImage(img, x, y, w, h)
+          ctx.restore()
+          resolve()
+        }
       }
-      Image.onerror = () => { reject() }
-      Image.src = img
+
+      const isTempFile = /^wxfile:\/\//.test(img)
+      const isNetworkFile = /^https?:\/\//.test(img)
+
+      if (isTempFile) {
+        _drawImage(img)
+      } else if (isNetworkFile) {
+        wx.downloadFile({
+          url: img,
+          success(res) {
+            if (res.statusCode === 200) {
+              _drawImage(res.tempFilePath)
+            } else {
+              reject(new Error(`downloadFile:fail ${img}`))
+            }
+          },
+          fail() {
+            reject(new Error(`downloadFile:fail ${img}`))
+          }
+        })
+      } else {
+        reject(new Error(`image format error: ${img}`))
+      }
     })
   }
 
@@ -575,12 +668,12 @@ class Draw {
       backgroundColor = 'transparent'
     } = style
 
+    if (typeof lineHeight === 'string') { // 2em
+      lineHeight = Math.ceil(parseFloat(lineHeight.replace('em')) * fontSize)
+    }
     if (!text || (lineHeight > h)) return
 
     ctx.save()
-    if (lineHeight) { // 2em
-      lineHeight = Math.ceil(parseFloat(lineHeight.replace('em')) * fontSize)
-    }
     ctx.textBaseline = 'top'
     ctx.font = `${fontSize}px sans-serif`
     ctx.textAlign = textAlign
