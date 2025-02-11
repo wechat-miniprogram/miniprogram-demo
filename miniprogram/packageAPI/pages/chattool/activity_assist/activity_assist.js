c0ce6b9d7373afd95e0c6a14874812b0301bffdb
sanfordsun <sanfordsun@tencent.com>
2025-02-11 20:33:34 +0800

    feat: cloud init

--- miniprogram/packageAPI/pages/chattool/activity_assist/activity_assist.js
+++ miniprogram/packageAPI/pages/chattool/activity_assist/activity_assist.js
@@ -1,3 +1,4 @@
+const config = require('../../../../config')
 const util = require('../../../../util/util')
 const systemInfo = wx.getSystemInfoSync()
 Page({
@@ -29,13 +30,9 @@ Page({
     }
 
     wx.cloud.init({
-      // env 参数说明：
-      //   env 参数决定接下来小程序发起的云开发调用（wx.cloud.xxx）会默认请求到哪个云环境的资源
-      //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
-      //   如不填则使用默认环境（第一个创建的环境）
-      env: "test-f0b102",
+      env: config.envId,
       traceUser: true,
-    });
+    })
     
     this.setData({
       theme: wx.getSystemInfoSync().theme || 'light'
