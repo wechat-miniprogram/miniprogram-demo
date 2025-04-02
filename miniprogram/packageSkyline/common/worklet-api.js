fb142d6073595d81c14f8414d3652d621195bcc7
v_guanglwen <v_guanglwen@tencent.com>
2025-04-02 14:20:24 +0800

    fix: code format

--- miniprogram/packageSkyline/common/worklet-api.js
+++ miniprogram/packageSkyline/common/worklet-api.js
@@ -1,7 +1,7 @@
 function foo() {}
 
 function bar() {
-  'worklet';
+  'worklet'
 }
 
 export const worklet = {
@@ -14,24 +14,24 @@ export const worklet = {
   timing: bar,
   decay: bar,
   Easing: {},
-};
+}
 
 export const router = {
   addRouteBuilder: foo,
   removeRouteBuilder: foo,
   getRouteContext: foo,
-};
+}
 
 if (wx.worklet) {
-  Object.assign(worklet, wx.worklet);
+  Object.assign(worklet, wx.worklet)
 }
 
 if (wx.router) {
-  Object.assign(router, wx.router);
+  Object.assign(router, wx.router)
 }
 
 export function supportWorklet() {
-  return typeof wx.worklet === 'object' && typeof wx.worklet.shared === 'function';
+  return typeof wx.worklet === 'object' && typeof wx.worklet.shared === 'function'
 }
 
 export function shared(args) {
