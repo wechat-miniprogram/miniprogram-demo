8192b068190c6318f9ddebc4d929ce5a15b90883
v_guanglwen <v_guanglwen@tencent.com>
2025-06-24 15:17:25 +0800

    fix: wx.createVKSession对齐文档

--- miniprogram/packageAPI/pages/ar/plane-ar-3dof/behavior.js
+++ miniprogram/packageAPI/pages/ar/plane-ar-3dof/behavior.js
@@ -96,7 +96,7 @@ export default function getBehavior() {
         const session = this.session = wx.createVKSession({
           track: {
             plane: {
-              mode: 3
+              mode: 1
             },
             threeDof: true,
           },
